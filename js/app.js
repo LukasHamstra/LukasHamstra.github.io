780d9fc2684ad84464b4efac6aaf49cf121f0b08
Lukas <lukashamstra@gmail.com>
2020-02-18 19:21:54 +0100

    Added contact form and the footer. Also added an up-arrow. Gotta see what we wanna do with that.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 $(function(){
-    console.log("Hello");
+    $('#scroll-top').hide();
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        if (scroll > 900) {
+            $('#scroll-top').show()
+        } else {
+            $('#scroll-top').hide();
+        }
+    });
+
+    $('#scroll-top').click(function() {
+        $('#scroll-top').hide();
+    })
 });
