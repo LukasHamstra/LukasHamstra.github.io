d2b00d4704e9dcbd777f8a692a557a6cf9bc2abb
Lukas Hamstra <LD.Hamstra@student.nl>
2020-02-20 13:16:58 +0100

    heb even gekeken naar tweetalig maken

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
+var lang = 'nl';
+
 $(function(){
+    loadText();
     $('#scroll-top').hide();
     $(window).scroll(function (event) {
         var scroll = $(window).scrollTop();
@@ -13,3 +16,20 @@ $(function(){
         $('#scroll-top').hide();
     })
 });
+
+function setLanguage(newLang) {
+    lang = newLang;
+    loadText();
+}
+
+function loadText() {
+    var url = 'LukasHamstra.github.io/assets/languages/' + lang + '.json';
+    $.getJSON(url, function(data) {
+        console.log(data);
+        $('#nav-home').html(data.navbar[0]);
+        $('#nav-services').html(data.navbar[1]);
+        $('#nav-portfolio').html(data.navbar[2]);
+        $('#nav-about').html(data.navbar[3]);
+        $('#nav-contact').html(data.navbar[4]);
+    });
+}
